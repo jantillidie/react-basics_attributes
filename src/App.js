c3ba8a646d4d876c823389fe2ebb8bb0d2077562
Jan Maaß <git@antipost.de>
2023-11-13 11:31:44 +0100

    Create Article function

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+export function Article() {
+  return <article className="article">
+    <h2 className="article__title">Article title</h2>
+    <label htmlFor="userInput">Enter the article:</label>
+    <input type="text" id="userInput" name="name" />
+    <a href="https://react.dev/" className="article__link">Article link</a>
+  </article>
 }
